321906329602edc3bc15b350d92a3a54d001da34
Eric Vogler <ericvogler@gmail.com>
2021-05-21 16:50:24 -0700

    Change basic server route to /

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
--- server/index.js
+++ server/index.js
@@ -6,12 +6,11 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
-app.get('/test', (req, res) => {
+app.get('/', (req, res) => {
   const pathName = path.join(__dirname, '..', 'client', 'dist', 'index.html');
-  console.log(pathName);
   res.sendFile(pathName);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`listening at http://localhost:${port}`)
 });
