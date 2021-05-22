b18c5e200f623321086113bdef011498dbc069e2
Eric Vogler <ericvogler@gmail.com>
2021-05-22 10:24:56 -0700

    Create dummy components for pages

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,6 +5,31 @@ import {
 } from "react-router-dom";
 import styled from 'styled-components';
 
+const LandingPage = () => {
+  return ( <div>LandingPage</div>);
+}
+const SignUp = () => {
+  return ( <div>SignUp</div>);
+}
+const LogIn = () => {
+  return ( <div>LogIn</div>);
+}
+const MealPlan = () => {
+  return ( <div>MealPlan</div>);
+}
+const Questionnaire = () => {
+  return ( <div>Questionnaire</div>);
+}
+const RecipeGenerator = () => {
+  return ( <div>RecipeGenerator</div>);
+}
+const GroceryStore = () => {
+  return ( <div>GroceryStore</div>);
+}
+const UserProfile = () => {
+  return ( <div>UserProfile</div>);
+}
+
 const App = () => {
   return (
     <div>Welcome to the future homepage of Health Food Inc.!</div>
