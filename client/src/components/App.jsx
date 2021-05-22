d968d041d1312bda5590ed265e3f34132dcad9de
Eric Vogler <ericvogler@gmail.com>
2021-05-21 19:52:53 -0700

    WIP installing Bulma

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,34 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import styled from 'styled-components';
+import './App.sass';
 
+const BigFont = styled.div`
+  font-size: 17px;
+`
+const Wrapper = styled(BigFont)`
+  color: red;
+`
 const App = () => {
   return (
-    <h2>Welcome to the future website of Health Food Inc!</h2>
+    <>
+      <Wrapper>
+        <h2>
+          Welcome to the future website of Health Food Inc!
+
+        </h2>
+      </Wrapper>
+      <section class="section">
+        <div class="container">
+          <h1 class="title">
+            Hello World
+          </h1>
+          <p class="subtitle">
+            My first website with <strong>Bulma</strong>!
+          </p>
+        </div>
+     </section>
+    </>
   );
 };
 
