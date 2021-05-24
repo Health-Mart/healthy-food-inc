d8b40d15e254dbc02c24123d1ffad1e35d524bfb
Eric Vogler <ericvogler@gmail.com>
2021-05-24 10:25:57 -0700

    Change GET recipes/ tags & restrictions params to not require JSON array syntax

--- server/controllers/recipeAPI.js
+++ server/controllers/recipeAPI.js
@@ -1,10 +1,14 @@
 const { getRecipes } = require('../models/stored-recipes')
 
+const parseArray = str => {
+  return str.split(',').map(s => s.trim());
+}
+
 exports.getRecipes = async (req, res) => {
   try {
     const query = req.query.query ?? '';
     const tags = req.query.tags ? JSON.parse(req.query.tags) : [];
-    const restrictions = req.query.tags ? JSON.parse(req.query.restrictions) : [];
+    const restrictions = req.query.restrictions ? parseArray(req.query.restrictions) : [];
     const count = req.query.count ?? 5;
     const offset = req.query.offset ?? 0;
 
