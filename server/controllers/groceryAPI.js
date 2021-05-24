1d72a502ab8dae08cf7a1e776be11502354a3248
Eric Vogler <ericvogler@gmail.com>
2021-05-23 18:34:58 -0700

    Implement getRecipes (and stored-recipes)

--- server/controllers/groceryAPI.js
+++ server/controllers/groceryAPI.js
@@ -1,6 +1,9 @@
-exports.getGroceryItems = async (req, res) => {
-  try {
+const { dataSource } = require('../settings.js');
 
+exports.getGroceryItems = async (req, res) => {
+  // -> [{id, name, keywords, description, image}...]
+  try {
+    // if (dataSource === 'json')
   } catch (err) {
     res.status(404).send(err.message);
   }
