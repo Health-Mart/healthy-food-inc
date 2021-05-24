05361b1a4c3c9cb78ab75d305351e9263c3c8b44
Eric Vogler <ericvogler@gmail.com>
2021-05-24 11:03:40 -0700

    Simplify query parsing in recipeAPI.js

--- server/controllers/recipeAPI.js
+++ server/controllers/recipeAPI.js
@@ -1,14 +1,11 @@
 const { getRecipes } = require('../models/stored-recipes')
 
-const parseArray = str => {
-  return str.split(',').map(s => s.trim());
-}
-
 exports.getRecipes = async (req, res) => {
   try {
+    console.log(req.url);
     const query = req.query.query ?? '';
-    const tags = req.query.tags ? JSON.parse(req.query.tags) : [];
-    const restrictions = req.query.restrictions ? parseArray(req.query.restrictions) : [];
+    const tags = req.query.tags ?? [];
+    const restrictions = req.query.restrictions ?? [];
     const count = req.query.count ?? 5;
     const offset = req.query.offset ?? 0;
 
