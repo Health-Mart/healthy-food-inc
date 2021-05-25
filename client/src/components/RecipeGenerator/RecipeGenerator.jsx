24046335b57d2a6b1639b4e8f37b17998cc2425d
Sim Neyzi <simneyzi@gmail.com>
2021-05-25 11:38:23 -0700

    mealplan v1

--- client/src/components/RecipeGenerator/RecipeGenerator.jsx
+++ client/src/components/RecipeGenerator/RecipeGenerator.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import styled from 'styled-components';
 import {
+<<<<<<< HEAD
   BrowserRouter as Router,
   Switch,
   Route,
@@ -11,6 +12,13 @@ import {
 import Hero from './Hero.jsx';
 import Column from './Column.jsx';
 import Footer from './Footer.jsx';
+=======
+  BrowserRouter as Router, Switch, Route, Link,
+  useLocation, useParams
+} from "react-router-dom";
+
+//import { HealthContext } from '../../context/healthContext.jsx';
+>>>>>>> 86f3513 (mealplan v1)
 
 function RecipeGenerator() {
   return (
