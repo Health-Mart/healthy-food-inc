86f3513df5c068fb5ce4c966b77e63fcc8b44457
Sim Neyzi <simneyzi@gmail.com>
2021-05-25 11:38:23 -0700

    mealplan v1

---
+++ client/src/components/MealPlan/SearchMeal.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect, useContext } from 'react';
+import bulma from 'bulma';
+import styled from 'styled-components';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useLocation,
+  useParams
+} from 'react-router-dom';
+
+function SearchMeal() {
+  return (
+  <div className="field is-grouped my-2 px-6">
+    <p className="control is-expanded">
+      <input className="input is-rounded" type="text" placeholder="What do you want to eat?" />
+    </p>
+    <p className="control">
+      <a className="button is-info">Search</a>
+    </p>
+  </div>
+  )
+}
+
+export default SearchMeal;
--- client/src/components/RecipeGenerator/RecipeGenerator.jsx
+++ client/src/components/RecipeGenerator/RecipeGenerator.jsx
@@ -5,11 +5,9 @@ import {
   useLocation, useParams
 } from "react-router-dom";
 
-import { HealthContext } from '../../context/healthContext.jsx';
+//import { HealthContext } from '../../context/healthContext.jsx';
 
 function RecipeGenerator() {
-  const { countIncrement } = useContext(HealthContext);
-  const [count, setCount] = countIncrement;
   return (
     <div>RecipeGenerator</div>
   )
