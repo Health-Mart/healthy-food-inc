8baea48214cd8d43956dd1bf6b01cbef36488eed
Eric Vogler <ericvogler@gmail.com>
2021-05-26 12:10:20 -0700

    WIP

--- server/controllers/mealPlansAPI.js
+++ server/controllers/mealPlansAPI.js
@@ -1,4 +1,6 @@
-exports.getMealPlans = async (req, res) => {
+const getMeals = require('../models/stored-meals.js');
+
+exports.getMealPlan = async (req, res) => {
   try {
 
   } catch (err) {
@@ -6,9 +8,14 @@ exports.getMealPlans = async (req, res) => {
   }
 }
 
-exports.getMealPlan = async (req, res) => {
+exports.getMealPlans = async (req, res) => {
+  console.log('GETMEALPLAN');
   try {
-
+    const key = JSON.stringify(req.query);
+    console.log(key);
+    getMeals(key).then(res => {
+      res.status(200).json(key);
+    });
   } catch (err) {
     res.status(404).send(err.message);
   }
---
+++ server/models/stored-meals.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const fs = require('fs/promises');
+
+const filename = path.join(__dirname, '..', 'stored-data', 'meals', 'meals.json');
+
+const allMeals = fs.readFile(filename, { encoding: 'utf8' })
+  .then(res => {
+    return JSON.parse(res);
+  });
+
+getMeals = ({ categoryName, subCategoryName, title, count = 5, offset = 0 } = {}) => {
+  return allMeals.then(meals => {
+    if (categoryName) {
+      categoryName = categoryName.toLowerCase();
+      meals = meals.filter(meal => meal.categoryName.toLowerCase() === categoryName);
+    }
+    if (subCategoryName) {
+      subCategoryName = subCategoryName.toLowerCase();
+      meals = meals.filter(meal => meal.subCategoryName.toLowerCase() === subCategoryName);
+    }
+    if (title) {
+      title = title.toLowerCase();
+      meals = meals.filter(meal => meal.title.indexOf(title) !== -1);
+    }
+    return meals.slice(offset, offset + count);
+  });
+}
+
+getMeals({ count: 5, subCategoryName: 'salads' })
+  .then(res => console.log(res[0].details.productDetails));
+
+module.exports = { getMeals };
