90b887a7569c08f9fc964f15d2818e134fda9db2
Calvin Hu <31859826+chu017@users.noreply.github.com>
2021-05-26 12:35:13 -0700

    recipe tweak

--- client/src/components/RecipeGenerator/Card.jsx
+++ client/src/components/RecipeGenerator/Card.jsx
@@ -29,9 +29,23 @@ const replacementImages = {
   661984: 'https://images-gmi-pmc.edge-generalmills.com/73bd4146-76dd-46ac-81e4-ee1cb9428143.jpg',
   665261:
     'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/11/23/0/FN_perfect-chicken-014_s4x3.jpg.rend.hgtvcom.616.462.suffix/1384540895467.jpeg',
-  715538: 'https://d1e3z2jco40k3v.cloudfront.net/-/media/mccormick-us/recipes/mccormick/c/800/chicken_bruschetta_pasta_800x800.jpg',
+  715538:
+    'https://d1e3z2jco40k3v.cloudfront.net/-/media/mccormick-us/recipes/mccormick/c/800/chicken_bruschetta_pasta_800x800.jpg',
   634698: 'https://andrewzimmern.com/wp-content/uploads/Beef-Tataki-1600x800.jpg',
-  665734: 'https://fruitsandveggies.org/wp-content/uploads/2018/11/Corn-zucchini-pan-omelet-5-square-web-600x274.jpg'
+  665734:
+    'https://fruitsandveggies.org/wp-content/uploads/2018/11/Corn-zucchini-pan-omelet-5-square-web-600x274.jpg',
+  652335: 'https://thecozyapron.com/wp-content/uploads/2018/05/mongolian-beef_thecozyapron_1.jpg',
+  797177: 'https://img.taste.com.au/s9qgdSXL/taste/2016/11/corned-beef-hash-cakes-14696-1.jpeg',
+  635793:
+    'https://lh3.googleusercontent.com/proxy/4UjS0e93eUmMB5XQQbpSbFg_29HVuPNoey1az4BatI0rNE2oLgOWOCD7UHrO9IMu3p1r7Bew4-C8qGQpRKJ7TVc5qxxPRcSLbJDBGZLLBYsmZc8kx-WIHmrMaF0rfRAxYUIU_HEyZlU',
+  390813:
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi20Onn2CiABwIPc04rLBRSSSiSTFcmTDqWg&usqp=CAU',
+  644711:
+    'http://www.simplybeefandlamb.co.uk/media/2071/festive-roast-beef-with-ginger-and-five-spice-butter.jpg?width=1600&height=900&mode=crop',
+  634710:
+    'https://jamiecooksitup.net/wp-content/uploads/2018/04/Teriyaki-Beef-and-Pepper-Stir-Fry2.jpg',
+  660273:
+    'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/slow-cooker-chili-horizontal-1529354330.png'
 };
 
 function Card({ item }) {
@@ -54,7 +68,7 @@ function Card({ item }) {
         </div>
         <div className="card-content">
           <p className="title is-5">{title}</p>
-          {/* <span>{item.id}</span> */}
+          <span>{item.id}</span>
           <p className="title is-7" />
         </div>
         <button onClick={openModal} className="button is-small" type="button">
