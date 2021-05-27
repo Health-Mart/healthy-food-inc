7c4df8023562b5738fa2331f9fb016e868f1ee02
Calvin Hu <31859826+chu017@users.noreply.github.com>
2021-05-26 22:11:04 -0700

    update eslint

--- client/src/components/RecipeGenerator/RecipeGenerator.jsx
+++ client/src/components/RecipeGenerator/RecipeGenerator.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect, useContext } from 'react';
 import styled from 'styled-components';
 import {
