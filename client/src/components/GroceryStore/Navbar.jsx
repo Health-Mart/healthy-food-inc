f00e904e5bb16fa9b10939d2312ca85ee105c9aa
Joseph Lio <josephlio@hotmail.com>
2021-05-27 21:43:25 -0700

    Bugfix grocery navbar syntax

--- client/src/components/GroceryStore/Navbar.jsx
+++ client/src/components/GroceryStore/Navbar.jsx
@@ -14,23 +14,6 @@ const Count = styled.span`
     padding-left: 3px;
 `;
 
-<<<<<<< HEAD
-const Nav = styled.nav`
-  position: sticky;
-  top: 0px;
-`;
-
-function Navbar({ mainCategory, setMainCategory, count }) {
-  return (
-    <Nav className="navbar is-success" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="/">
-          <div>
-            <strong>FARMER MARKET</strong>
-          </div>
-        </a>
-      </div>
-=======
 
 
 function Navbar({ mainCategory, setMainCategory, count }) {
@@ -44,88 +27,88 @@ function Navbar({ mainCategory, setMainCategory, count }) {
             </div>
           </a>
         </div>
->>>>>>> 795e9b8540ca7a311a55912c9b6d655fa62e561a
 
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <a
-            className={`navbar-item ${mainCategory === 'Produce' ? 'is-tab is-active' : ''}`}
-            onClick={() => setMainCategory('Produce')}
-          >
-            Produce
-          </a>
-          <a
-            className={`navbar-item ${mainCategory === 'Dairy' ? 'is-tab is-active' : ''}`}
-            onClick={() => setMainCategory('Dairy')}
-          >
-            Dairy
-          </a>
-          <a
-            className={`navbar-item ${mainCategory === 'Meat & Fish' ? 'is-tab is-active' : ''}`}
-            onClick={() => setMainCategory('Meat & Fish')}
-          >
-            Meat & Fish
-          </a>
-          <a
-            className={`navbar-item ${mainCategory === 'Alcohol' ? 'is-tab is-active' : ''}`}
-            onClick={() => setMainCategory('Alcohol')}
-          >
-            Alcohol
-          </a>
-          <a
-            className={`navbar-item ${mainCategory === 'Prepared' ? 'is-tab is-active' : ''}`}
-            onClick={() => setMainCategory('Prepared')}
-          >
-            Prepared
-          </a>
-          <a
-            className={`navbar-item ${mainCategory === 'Snacks' ? 'is-tab is-active' : ''}`}
-            onClick={() => setMainCategory('Snacks')}
-          >
-            Snacks
-          </a>
-          <a
-            className={`navbar-item ${mainCategory === 'Drinks' ? 'is-tab is-active' : ''}`}
-            onClick={() => setMainCategory('Drinks')}
-          >
-            Drinks
-          </a>
-          <a
-            className={`navbar-item ${mainCategory === 'Pantry' ? 'is-tab is-active' : ''}`}
-            onClick={() => setMainCategory('Pantry')}
-          >
-            Pantry
-          </a>
-          <a
-            className={`navbar-item ${
-              mainCategory === 'Health & Home' ? 'is-tab is-active' : ''
-            }`}
-            onClick={() => setMainCategory('Health & Home')}
-          >
-            Health & Home
-          </a>
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start">
+            <a
+              className={`navbar-item ${mainCategory === 'Produce' ? 'is-tab is-active' : ''}`}
+              onClick={() => setMainCategory('Produce')}
+            >
+              Produce
+            </a>
+            <a
+              className={`navbar-item ${mainCategory === 'Dairy' ? 'is-tab is-active' : ''}`}
+              onClick={() => setMainCategory('Dairy')}
+            >
+              Dairy
+            </a>
+            <a
+              className={`navbar-item ${mainCategory === 'Meat & Fish' ? 'is-tab is-active' : ''}`}
+              onClick={() => setMainCategory('Meat & Fish')}
+            >
+              Meat & Fish
+            </a>
+            <a
+              className={`navbar-item ${mainCategory === 'Alcohol' ? 'is-tab is-active' : ''}`}
+              onClick={() => setMainCategory('Alcohol')}
+            >
+              Alcohol
+            </a>
+            <a
+              className={`navbar-item ${mainCategory === 'Prepared' ? 'is-tab is-active' : ''}`}
+              onClick={() => setMainCategory('Prepared')}
+            >
+              Prepared
+            </a>
+            <a
+              className={`navbar-item ${mainCategory === 'Snacks' ? 'is-tab is-active' : ''}`}
+              onClick={() => setMainCategory('Snacks')}
+            >
+              Snacks
+            </a>
+            <a
+              className={`navbar-item ${mainCategory === 'Drinks' ? 'is-tab is-active' : ''}`}
+              onClick={() => setMainCategory('Drinks')}
+            >
+              Drinks
+            </a>
+            <a
+              className={`navbar-item ${mainCategory === 'Pantry' ? 'is-tab is-active' : ''}`}
+              onClick={() => setMainCategory('Pantry')}
+            >
+              Pantry
+            </a>
+            <a
+              className={`navbar-item ${
+                mainCategory === 'Health & Home' ? 'is-tab is-active' : ''
+              }`}
+              onClick={() => setMainCategory('Health & Home')}
+            >
+              Health & Home
+            </a>
 
 
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link">More</a>
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link">More</a>
 
-            <div className="navbar-dropdown">
-              <a className="navbar-item" href="/mealplan">Meal Plan</a>
-              <a className="navbar-item" href="/recipe-generator">Recipe Generator</a>
-              <a className="navbar-item" href="/questionnaire">Questionnaire</a>
+              <div className="navbar-dropdown">
+                <a className="navbar-item" href="/mealplan">Meal Plan</a>
+                <a className="navbar-item" href="/recipe-generator">Recipe Generator</a>
+                <a className="navbar-item" href="/questionnaire">Questionnaire</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <a className="button is-light">Shopping Bag <Count>({count})</Count></a>
+              </div>
             </div>
           </div>
         </div>
-
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <a className="button is-light">Shopping Bag <Count>({count})</Count></a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Nav>
+      </nav>
+    </>
   );
 }
 
